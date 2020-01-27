e7d9691824c294ea146396505bcf5d1597332d61
Abdullah <abdullah@gamatechno.com>
2020-01-27 12:59:37 +0700

    Update thirdparty view (AvatarView, EmphasisTextView, Animation)

---
+++ mylibs/src/main/java/lib/gmsframeworkx/utils/EmphasisTextView/HighlightMode.java
@@ -0,0 +1,15 @@
+package lib.gmsframeworkx.utils.EmphasisTextView;
+
+public enum HighlightMode {
+
+    /**
+     * Highlight the background of text
+     */
+    BACKGROUND,
+
+    /**
+     * Change the color of the text instead of that of the background.
+     */
+    TEXT
+
+}
